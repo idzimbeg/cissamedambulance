37d110823fc95a8f10d76613e1ae065b09fccf13
Igor Dzimbeg <igor.dzimbeg@init.hr>
2022-07-25 09:17:50 +0200

    Change route options

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './language/i18';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
 
